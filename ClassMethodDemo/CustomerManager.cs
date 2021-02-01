bfb2cc6527f8a14b5f0b3fffe8cf6d441ea8695b
ece özaydın <ozaydinecee@gmail.com>
2021-02-01 21:50:26 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine("Customer added: "+customer.CustomerName+" " + customer.CustomerLastName+ "  Id:"+customer.CustomerId);
+        }
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted:" + customer.CustomerName + " " + customer.CustomerLastName + "Id:" + customer.CustomerId);
+        }
+        public void CustomerList(List<Customer> customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.CustomerName + " " + customer.CustomerLastName);
+            }
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "ece";
+            customer1.CustomerLastName = "ozaydin";
+
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "yildiz";
+            customer2.CustomerLastName = "ozaydin";
+
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 3;
+            customer3.CustomerName = "elif";
+            customer3.CustomerLastName = "ozaydin";
+
+
+
+
+            CustomerManager customerManager1 = new CustomerManager();
+            customerManager1.CustomerAdd(customer1);
+            customerManager1.CustomerAdd(customer2);
+            customerManager1.CustomerAdd(customer3);
+
+            Console.WriteLine("-----Customer List--------"); 
+
+            List<Customer> listCustomer = new List<Customer> { customer1, customer2, customer3 };
+            customerManager1.CustomerList(listCustomer);
+
+            Console.ReadLine();
+        }
+    }
+}
